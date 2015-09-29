76cc10048c118b4b2e483a843e14a0e622a630f7
bcooney <bcooney@tutanota.de>
2015-09-29 13:41:01 +0800

    Create if-elif-example.py

---
+++ if-elif-example.py
@@ -0,0 +1,11 @@
+name = 'bob'  #if it's alice, it will print if statement
+age = 102 
+if name == 'Alice':
+    print('hi alice')
+elif age < 12:
+    print('you are not alice')
+elif age > 2000:
+    print('you are so old')
+elif age > 100:
+    print('you are not alice, old lady')
+    
