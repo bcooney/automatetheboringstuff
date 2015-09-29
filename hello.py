7f7e1017273e9fc6c20ab22daa779458ed53a2a0
bcooney <bcooney@tutanota.de>
2015-09-29 13:16:49 +0800

    hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+# This program says hello and asks for my name
+
+print('hello world!')
+
+print('what is your name?') # ask for their name
+myName = input()
+print('good to meet you, ' + myName)
+print ('the length of your name is:')
+print (len(myName))
+
+print ('what is your age?') # ask for their age
+myAge = input()
+print ('you will be ' + str(int(myAge) + 1) +' in a year')  
