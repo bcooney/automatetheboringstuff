f328191f363dd4cd03d9d92a0959ce54a59a5a96
bcooney <bcooney@tutanota.de>
2015-09-29 13:41:21 +0800

    Create if-else-example.py

---
+++ if-else-example.py
@@ -0,0 +1,5 @@
+password = 'swrdfish'       #if the condition is true, if block executes
+if password == 'swordfish':
+    print('access granted')
+else:                       #if the condition is false, else block executes
+    print('wrong password')
