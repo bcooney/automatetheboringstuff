58d390721740fa94e1047ee3c5a9cf33bb73e658
bcooney <bcooney@tutanota.de>
2015-10-01 10:59:10 +0800

    Create jimmy.py

---
+++ jimmy.py
@@ -0,0 +1,9 @@
+print('my name is')
+# doing it with a while loop instead
+# i = 0
+# while i < 5:
+for i in range(5):
+# for i in range (5, 10): 2 criteria
+# for i in range (6, -2, -2): 3 criteria
+    print('jimmy five times ' + str(i))
+    # i = i + 1 required for while loop
